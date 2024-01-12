714f370967ec3e188909b155915fc82312ba1bde
Gregorio Vecchio <gregoriovecchio@gmail.com>
2024-01-12 03:32:25 +0100

    piccola modifica

--- netflix-react/src/components/Galleria.jsx
+++ netflix-react/src/components/Galleria.jsx
@@ -1,69 +1,5 @@
-/* import React, { Component } from 'react'
-import { Container, Spinner, Alert,} from 'react-bootstrap'
-import scrolla from './scrolla';
-
-export default class UsersComponent extends Component {
-
-state = {
-    users: [],
-    isLoading: false,
-    errMsg: false
-}
-
-
-
-componentDidMount() {
-    this.setState({
-      isLoading: true
-    });
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setTimeout(() => {
-          this.setState({
-            users: data,
-            isLoading: false
-          });
-        }, 2000);
-      })
-      .catch(error => {
-        console.error(error);
-        this.setState({
-          isLoading: false,
-          errMsg: true
-        });
-      });
-  }
-
-  render() {
-    return (
-      <Container>
-      <h1>User List</h1>
-      
-      {this.state.isLoading && <Spinner animation="border" />}
-      
-      {this.state.errMsg &&
-        <Alert variant="warning">
-          Si è verificato un errore nel caricamento dei dati
-        </Alert> 
-      }
-
-        <div>
-          {this.state.users.map(u => (
-            <h1 key={u.id} >
-              {u.name}
-            </h1>
-          ))}
-        </div>
-    </Container>
-    )
-  }
-}
- */
-
 import React, { Component } from 'react'
-import {Container, Spinner, Alert, } from 'react-bootstrap'
+import { Container, Spinner, Alert, } from 'react-bootstrap'
 import { urlApi } from '../dati/api';
 import scrolla from './scrolla';
 
@@ -73,7 +9,7 @@ export default class Galleria extends Component {
         movies: [],
         isLoading: false,
         errMsg: false,
-        sagaContratta:this.props.saga.replace(/[^a-zA-Z]/g, '')
+        sagaContratta: this.props.saga.replace(/[^a-zA-Z]/g, '')
     }
 
     componentDidMount() {
@@ -87,11 +23,11 @@ export default class Galleria extends Component {
             .then(json => {
                 console.log(json.Search);
                 this.setState({
-                    movies: json.Search.filter(ele => (ele.Poster !== "N/A")&&(ele.Type === "movie" || ele.Type === "series")),
+                    movies: json.Search.filter(ele => (ele.Poster !== "N/A") && (ele.Type === "movie" || ele.Type === "series")),
                     isLoading: false
                 }, () => {
                     console.log(this.state.movies)
-                });                
+                });
             })
             .catch(error => {
                 console.error(error);
@@ -106,17 +42,18 @@ export default class Galleria extends Component {
         console.log("render galleria");
         return (
             <Container>
-                {this.state.isLoading && <Spinner animation="border" />}
-                {this.state.errMsg &&
-                    <Alert variant="warning">
-                        Si è verificato un errore nel caricamento dei dati
-                    </Alert>
-                }
+
                 <div className="grigliaFilm my-4">
                     <h3 className="h4">{this.props.saga}</h3>
                     <div id={this.state.sagaContratta} className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-inner">
                             <div className="carousel-item active d-flex">
+                                {this.state.isLoading && <Spinner className='mx-auto' animation="border" />}
+                                {this.state.errMsg &&
+                                    <Alert className='mx-auto' variant="warning">
+                                        Si è verificato un errore nel caricamento dei dati
+                                    </Alert>
+                                }
                                 {this.state.movies.map(film => <a href="#" key={film.imdbID}><img src={film.Poster} alt={film.Title} /></a>)}
                             </div>
                         </div>
